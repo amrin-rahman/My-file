16b968f2c89263ee56d9469cc49e20463108b94c
amrin-rahman <amrin46896@gmail.com>
2025-11-22 21:24:20 +0600

    added by me

---
+++ Abstraction.java
@@ -0,0 +1,22 @@
+abstract class Animal {
+  // Abstract method (does not have a body)
+  public abstract void animalSound();
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+}
+
+// Subclass (inherit from Animal)
+class Pig extends Animal {
+  public void animalSound() {
+    System.out.println("The pig says: wee wee");
+  }
+}
+
+class Abstraction {
+  public static void main(String[] args) {
+    Pig myPig = new Pig(); // Create a Pig object
+    myPig.animalSound();
+    myPig.sleep();
+  }
+}
